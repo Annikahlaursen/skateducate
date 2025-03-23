664a1f891bb217afbb626b4d28e8eccadac3212d
Annikahlaursen <Annikahlaursen@gmail.com>
2025-03-23 21:54:13 +0100

    en del opdateringer
    omos
    forside
    sessions

---
+++ js.js
@@ -0,0 +1,15 @@
+// file: js.js
+// author: Annika H. Laursen
+// version: 1.0
+// date: 23-03-2025
+
+// navigationsbar, der får den til at fylde hele skærmen, når den foldes ud -->
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+      
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  AOS.init();
